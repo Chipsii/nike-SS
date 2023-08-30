262867f01b32226eef1e59da5ec2e83e89765560
Hasib <hasibfire24@gmail.com>
2023-08-30 20:02:37 +0600

    added css files, images and components

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+
+
+export {
+    Hero,
+    Footer,
+    Subscribe,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+};
